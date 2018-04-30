347e0432b81e896ab9a448186af3594db4ac0aea
olzhas2102 <olzhas2102@yahoo.com>
2018-04-30 21:49:54 +0200

    Adding ingredients

--- src/components/AddShake.vue
+++ src/components/AddShake.vue
@@ -8,9 +8,10 @@
       </div>
       <div class="field add-ingredient">
         <label for="add-ingredient">Add an ingredient:</label>
-        <input type="text" name="add-ingredient">
+        <input type="text" name="add-ingredient" @keydown.tab.prevent="addIng" v-model="another">
       </div>
       <div class="field center-align">
+        <p v-if="feedback" class="red-text">{{ feedback }}</p>
         <button class="btn green accent-4">Add Samurai Shake</button>
       </div>
     </form>
@@ -22,12 +23,25 @@ export default {
   name: 'AddShake',
   data() {
     return {
-      title: null
+      title: null,
+      another: null,
+      ingredients: [],
+      feedback: null
     }
   },
   methods: {
     AddShake() {
-      console.log(this.title)
+      console.log(this.title, this.ingredients)
+    },
+
+    addIng() {
+      if(this.another) {
+        this.ingredients.push(this.another)
+        this.another = null
+        this.feedback = null
+      } else {
+        this.feedback = 'Please, add an ingredient to complete the shake'
+      }
     }
   }
 }
