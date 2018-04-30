70e8d133f4eb404651e1019b4e74ca322ecc5f54
olzhas2102 <olzhas2102@yahoo.com>
2018-04-30 20:36:03 +0200

    Components, basic styling

---
+++ src/components/Index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="index container">
+    <div class="card blue-grey darken-4" v-for="shake in shakes" :key="shake.id">
+      <div class="card-content white-text">
+        <i class="material-icons delete" @click="deleteShake(shake.id)">delete</i>
+        <h2>{{ shake.title }}</h2>
+        <ul class="ingredients">
+          <li v-for="(ing, index) in shake.ingredients" :key="index">
+            <span class="chip">{{ ing }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      shakes: [
+        { title: 'Sweet Samurai', slug: 'sweet-samurai', ingredients: ['bananas', 'ice cream', 'oreo'], id: '1'},
+        { title: 'Samurai Breakfast', slug: 'samurai-breakfast', ingredients: ['coffee', 'ice cream', 'dark choco'], id: '2'}
+      ]
+    }
+  },
+  methods: {
+    deleteShake(id) {
+      this.shakes = this.shakes.filter(shake => {
+        return shake.id !== id
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.index {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 30px;
+  margin-top: 60px;
+}
+
+.index h2 {
+  font-size: 1.8em;
+  text-align: center;
+  margin-top: 0;
+}
+
+.index .ingredients {
+  margin: 30px auto;
+}
+
+.index .ingredients li {
+  display: inline-block;
+}
+
+.index .delete {
+  position: absolute;
+  top: 7px;
+  right: 7px;
+  cursor: pointer;
+  color: #EF8354;
+  font-size: 1.4em;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="navbar">
+    <nav class=" nav-extended amber darken-2">
+      <div class="nav-content">
+        <router-link to="">
+          <span class="nav-title">Samurai Shakes</span>
+        </router-link>
+        <a class="btn-floating btn-large halfway-fab blue-grey darken-4">
+          <router-link to="">
+            <i class="material-icons">add</i>
+          </router-link>
+        </a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+.navbar nav {
+  padding: 0 20px;
+}
+</style>
+
+
